019a0f3680b67b3a1c0145e6f9806ae6ef8257d8
Romargera <tolkohim@gmail.com>
2026-04-18 15:29:29 +0200

    feat: serve root URL and publish sitemap

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   publicDir: '../public',
-  base: '/me/',
+  base: '/',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
