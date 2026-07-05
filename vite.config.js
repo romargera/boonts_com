a0f65af19118b4490b69b48b85b1b4faf03f8b27
Romargera <tolkohim@gmail.com>
2026-07-05 22:26:04 +0200

    feat: add shesafe landing page under /shesafe

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
@@ -7,5 +8,11 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        shesafe: resolve(__dirname, 'src/shesafe/index.html'),
+      },
+    },
   },
 });
